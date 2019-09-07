d3d71e95c431d7923644425a1da445299178d4e7
socializemeserver <51453364+socializemeserver@users.noreply.github.com>
2019-09-07 19:05:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('TUTORIAL is ready');
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', (message) => {
+
+//commands goes here
+
+});
+
+client.login(process.env.TOKEN);
